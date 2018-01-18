d777bdfdf6cf69441892c22eb6126af8c1badd32
venerjoel99 <84veneracionj@gmail.com>
2018-01-17 23:30:54 -0500

    Add files via upload

---
+++ morse/morse.h
@@ -0,0 +1,76 @@
+#ifndef MORSE_H_INCLUDED
+#define MORSE_H_INCLUDED
+
+const int A = int('A');
+const int a = int('a');
+const int alphabet = 26;
+const int zero = int('0');
+
+String letters[]={
+  "._",   //A
+  "_...", //B
+  "_._.", //C
+  "_..",  //D
+  ".",    //E
+  ".._.", //F
+  "__.",  //G
+  "....", //H
+  "..",   //I
+  ".___", //J
+  "_._",  //K
+  "._..", //L
+  "__",   //M
+  "_.",   //N
+  "___",  //O
+  ".__.", //P
+  "__._", //Q
+  "._.",  //R
+  "...",  //S
+  "_",    //T
+  ".._",  //U
+  "..._", //V
+  ".__",  //W
+  "_.._", //X
+  "_.__", //Y
+  "__..", //Z
+};
+
+bool letter(int val){
+  return val >= A && val <= A + alphabet - 1;
+}
+
+bool lowercase(char input){
+  int val = int(input);
+  return val >= a && val <= a + alphabet - 1;
+}
+
+int digit(char input){
+  return int(input) - zero;
+}
+
+String morse(int input){
+  bool dot = (input >= 1 && input <= 5);
+  int count_down = input % 5;
+  String result = "";
+  for (int i = 0; i < 5; i++){
+    result += dot ? '.' : '_';
+    count_down--;
+    if (count_down==0){
+      dot = !dot;
+    }
+  }
+  return result;
+}
+
+String morse(char input){
+  int digit_val = digit(input);
+  if (digit_val >= 0 && digit_val <= 9){
+    Serial.println(digit_val);
+    return morse(digit_val)+';';
+  }
+  int index = lowercase(input) ? int(input) - (a-A) : int(input);
+  String morse = letter(index) ? letters[index - A] : "";
+  return morse + ';';
+}
+
+#endif
---
+++ morse/morse.ino
@@ -0,0 +1,66 @@
+#include "morse.h"
+
+const int led = 13;
+const int UNIT = 200;
+const int DOT = 1;
+const int LETTER_END = 2;
+const int DASH = 3;
+const int SPACE = 4;
+void lights(int units){
+  digitalWrite(led, HIGH);
+  delay(units*UNIT);
+  digitalWrite(led, LOW);
+  delay(UNIT);
+}
+
+void transmit(char symbol){
+  switch(symbol){
+    case '.':
+      lights(DOT);
+      break;
+    case '_':
+      lights(DASH);
+      break;
+    case ';':
+      delay(LETTER_END*UNIT);
+      break;
+    default:
+      delay(SPACE*UNIT);
+  }
+}
+
+void transmit(String code){
+  for (int i = 0; i < code.length(); i++){
+    transmit(code[i]);
+  }
+}
+
+void parse(char* fileName){
+  
+}
+
+/**
+ * Put setup code here
+ * Set LED pin as output
+ */
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+/**
+ * Put looping code here
+ * Blinks on and off half a second
+ */
+void loop() {
+  if (Serial.available() > 0){
+    char a = Serial.read();
+    if (a==' '){
+      transmit(' ');
+    }
+    else{
+      transmit(morse(a));
+    }    
+  }
+}
